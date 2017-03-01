057277239c4d424ee42c3553b6f2fb5574d69a7e
yangyafei <342363079@qq.com>
2017-03-01 15:11:57 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,7 @@
+require('../style/main.less');
+
+var sub = require('./sub');
+var app = document.createElement('div');
+app.innerHTML = '<h1>Hello World</h1>';
+app.appendChild(sub());
+document.body.appendChild(app);
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+//定义了一些文件夹的路径
+var ROOT_PATH = path.resolve(__dirname);
+var APP_PATH = path.resolve(ROOT_PATH, 'app');
+var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
+var STYLE_PATH = path.resolve(ROOT_PATH, 'style');
+
+module.exports = {
+    //项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
+    entry: APP_PATH,
+    //输出的文件名 合并以后的js会命名为bundle.js
+    output: {
+        path: BUILD_PATH,
+        filename: 'bundle.js'
+    },
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+        inline: true,
+        progress: true,
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.less$/,
+                loaders: ['style-loader','css-loader','less-loader'],
+                include: STYLE_PATH
+            }
+        ]
+    },
+    plugins: [
+
+    ]
+};
