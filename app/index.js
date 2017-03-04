41b5309b0fd3fc8d884cb7f4b80a36b7d30922ef
yangyafei <342363079@qq.com>
2017-03-04 17:35:54 +0800

    不能热更新

--- app/index.js
+++ app/index.js
@@ -1,7 +1,13 @@
-require('../style/main.less');
+// var $ = require('jquery');
+var moment = require('moment');
 
+require('../style/main.less');
 var sub = require('./sub');
+
 var app = document.createElement('div');
-app.innerHTML = '<h1>Hello World</h1>';
+app.innerHTML = '<h1>Hello feifei</h1>';
 app.appendChild(sub());
-document.body.appendChild(app);
+
+// 将下面js改为jq
+// document.body.appendChild(app);
+$('body').append('<p>look st me ! wenow is' +moment().format() + '</p>');
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+var webpack = require('webpack');
 var path = require('path');
 //定义了一些文件夹的路径
 var ROOT_PATH = path.resolve(__dirname);
@@ -6,6 +7,11 @@ var BUILD_PATH = path.resolve(ROOT_PATH, 'build');
 var STYLE_PATH = path.resolve(ROOT_PATH, 'style');
 
 module.exports = {
+    devtool: 'source-map',  //方便打包的文件查错，该参数只能用于开发环境，生产环境要换一个参数
+    // watch: true,
+    // watchOptions: {
+    //     poll: true
+    // },  //实时监听,不能实现hot replace，比较慢
     //项目的文件夹 可以直接用文件夹名称 默认会找index.js 也可以确定是哪个文件名字
     entry: APP_PATH,
     //输出的文件名 合并以后的js会命名为bundle.js
@@ -13,11 +19,12 @@ module.exports = {
         path: BUILD_PATH,
         filename: 'bundle.js'
     },
+
     devServer: {
         historyApiFallback: true,
         hot: true,
         inline: true,
-        progress: true,
+        progress: true
     },
     module: {
         loaders: [
@@ -25,10 +32,20 @@ module.exports = {
                 test: /\.less$/,
                 loaders: ['style-loader','css-loader','less-loader'],
                 include: STYLE_PATH
+            },
+            {
+                test: /\.(png|jpg)$/,
+                loader: 'url-loader?limit=40000'
             }
         ]
     },
     plugins: [
+        new webpack.ProvidePlugin({  //使用本插件可以不必在每个js文件中require一次jquery
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery"
+        }),
+        // new webpack.HotModuleReplacementPlugin()
 
     ]
 };
